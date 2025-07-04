12a50dc4f5a7eba99be2698a80d98b55f2e013fd
Phongnguyen1996 <phongndbps35129@fpt.edu.vn>
2025-07-04 23:19:42 +0700

    done bottom navigation bar

--- app/src/main/java/com/example/android_project/activity/MainActivity.java
+++ app/src/main/java/com/example/android_project/activity/MainActivity.java
@@ -28,13 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
-    // Khai báo các Fragment của bạn
-    // Bạn nên khởi tạo chúng một lần và tái sử dụng để giữ trạng thái
-    final Fragment fragmentHome = new HomeFragment(); // Thay thế bằng Fragment của bạn
-    final Fragment fragmentDashboard = new CartFragment(); // Thay thế bằng Fragment của bạn
-    final Fragment fragmentNotifications = new NotificationFragment(); // Thay thế bằng Fragment của bạn
+
+    final Fragment fragmentHome = new HomeFragment();
+    final Fragment fragmentCart = new CartFragment();
+    final Fragment fragmentNotifications = new NotificationFragment();
     final Fragment fragmentProfile = new ProfileFragment();
-    // ... Khai báo các fragment khác nếu có
 
     private Fragment activeFragment = fragmentHome;
 
@@ -53,8 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, fragmentProfile, "4").hide(fragmentProfile).commit();
         fragmentManager.beginTransaction().add(R.id.fragment_container, fragmentNotifications, "3").hide(fragmentNotifications).commit();
-        fragmentManager.beginTransaction().add(R.id.fragment_container, fragmentDashboard, "2").hide(fragmentDashboard).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, fragmentCart, "2").hide(fragmentCart).commit();
 
         fragmentManager.beginTransaction().add(R.id.fragment_container, fragmentHome, "1").commit();
 
@@ -63,38 +62,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
-                String tag = "";
+                // String tag = ""; // Biến tag có vẻ không được sử dụng ở đây, bạn có thể bỏ đi nếu không cần thiết
 
                 int itemId = item.getItemId();
-                if (itemId == R.id.bottom_navigation) {
+
+                // Sử dụng switch-case để xử lý các mục menu
+                // **QUAN TRỌNG:** Thay thế R.id.navigation_home, R.id.navigation_cart, v.v.
+                // bằng các ID thực tế từ file menu XML của bạn.
+                if (itemId == R.id.nav_home) { // Giả sử ID cho Home là navigation_home
                     selectedFragment = fragmentHome;
-                    tag = "1";
-                } else if (itemId == R.id.bottom_navigation) {
-                    selectedFragment = fragmentDashboard;
-                    tag = "2";
-                } else if (itemId == R.id.bottom_navigation) {
+                    // tag = "1";
+                } else if (itemId == R.id.nav_cart) { // Giả sử ID cho Cart là navigation_cart
+                    selectedFragment = fragmentCart; // fragmentDashboard có lẽ nên là fragmentCart
+                    // tag = "2";
+                } else if (itemId == R.id.nav_notifications) { // Giả sử ID cho Notifications là navigation_notifications
                     selectedFragment = fragmentNotifications;
-                    tag = "3";
-                } else{
+                    // tag = "3";
+                } else if (itemId == R.id.nav_profile) { // Giả sử ID cho Profile là navigation_profile
                     selectedFragment = fragmentProfile;
-                    tag = "4";
+                    // tag = "4";
+                } else {
+                    // Trường hợp không có ID nào khớp (có thể không cần thiết nếu tất cả các item đều được xử lý)
+                    return false;
                 }
 
+
+                // Logic để hiển thị Fragment đã chọn
                 if (selectedFragment != null && selectedFragment != activeFragment) {
-                    // Ẩn fragment hiện tại, hiển thị fragment mới
-                    fragmentManager.beginTransaction().hide(activeFragment).show(selectedFragment).commit();
+                    // Bạn cần khai báo fragmentManager ở cấp độ class hoặc lấy lại ở đây
+                    // FragmentManager fragmentManager = getSupportFragmentManager(); // Nếu chưa khai báo ở cấp class
+                    getSupportFragmentManager().beginTransaction().hide(activeFragment).show(selectedFragment).commit();
                     activeFragment = selectedFragment;
                     return true;
                 }
-                return false;
+                return false; // Trả về false nếu Fragment đã được chọn hoặc không có Fragment nào được chọn
             }
         });
 
-        // Đặt mục được chọn mặc định (nếu cần)
-        // bottomNavigationView.setSelectedItemId(R.id.navigation_home); // Ví dụ
     }
 
-    // (Optional) Bạn có thể dùng phương thức này nếu muốn chuyển Fragment từ nơi khác
     private void loadFragment(Fragment fragment) {
         if (fragment != null && fragment != activeFragment) {
             getSupportFragmentManager()
